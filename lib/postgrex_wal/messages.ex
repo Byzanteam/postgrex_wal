305ecdc2736fbeba8624ff881a5a6542db7280cb
GloryHonor <jminfo@126.com>
2022-12-12 14:26:38 +0800

    feat: add 4 more dispatching branches

--- lib/postgrex_wal/messages.ex
+++ lib/postgrex_wal/messages.ex
@@ -1,14 +1,18 @@
 defmodule PostgrexWal.Messages do
   @moduledoc false
 
-  alias PostgrexWal.Message.{Begin, Commit, Relation, Insert, Delete}
+  alias PostgrexWal.Message.{Begin, Commit, Relation, Insert, Delete, Truncate, Type, Update, Origin}
   def decode(<<key::binary-1, rest::binary>>) do
     modules = %{
       "B" => Begin,
       "C" => Commit,
       "R" => Relation,
       "I" => Insert,
-      "D" => Delete
+      "D" => Delete,
+      "T" => Truncate,
+      "Y" => Type,
+      "U" => Update,
+      "O" => Origin
     }
     apply(modules[key], :decode, [rest])
   end

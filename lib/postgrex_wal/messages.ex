47ab421e2194a90ec415c37a196e5addeff6ff4f
GloryHonor <jminfo@126.com>
2022-12-12 19:07:12 +0800

    fix: integer-match instead of binary-match

--- lib/postgrex_wal/messages.ex
+++ lib/postgrex_wal/messages.ex
@@ -1,18 +1,6 @@
 defmodule PostgrexWal.Messages do
   @moduledoc false
 
-  @modules %{
-    "B" => Begin,
-    "C" => Commit,
-    "D" => Delete,
-    "I" => Insert,
-    "O" => Origin,
-    "R" => Relation,
-    "T" => Truncate,
-    "Y" => Type,
-    "U" => Update
-  }
-
   alias PostgrexWal.Message.{
     Begin,
     Commit,
@@ -25,8 +13,20 @@ defmodule PostgrexWal.Messages do
     Update
   }
 
+  @modules %{
+    ?B => Begin,
+    ?C => Commit,
+    ?D => Delete,
+    ?I => Insert,
+    ?O => Origin,
+    ?R => Relation,
+    ?T => Truncate,
+    ?Y => Type,
+    ?U => Update
+  }
+
   for {key, module} <- @modules do
-    def decode(<<unquote(key)::binary-1, payload::binary>>) do
+    def decode(<<unquote(key)::8, payload::binary>>) do
       unquote(module).decode(payload)
     end
   end

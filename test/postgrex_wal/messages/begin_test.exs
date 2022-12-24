8d602b5016202b019818416c84760a7f0f8f83e6
Jason <jminfo@126.com>
2022-12-24 21:55:18 +0800

    test: more tests in test-case. (begin_test.exs, update_test.exs)

--- test/postgrex_wal/messages/begin_test.exs
+++ test/postgrex_wal/messages/begin_test.exs
@@ -1,14 +1,26 @@
 defmodule PostgrexWal.Messages.BeginTest do
   use ExUnit.Case, async: true
   alias PostgrexWal.Messages.Begin
-  @event <<0, 0, 0, 0, 2, 227, 251, 232, 0, 2, 146, 234, 159, 209, 83, 66, 0, 0, 13, 247>>
 
+  @event <<0, 0, 0, 0, 2, 227, 251, 232, 0, 2, 146, 234, 159, 209, 83, 66, 0, 0, 13, 247>>
   test "decode begin event" do
     assert match?(
              %Begin{
                transaction_id: 3_575,
                commit_timestamp: ~U[2022-12-16 06:12:34.719554Z],
                final_lsn: {0, 48_495_592}
+             },
+             Begin.decode(@event)
+           )
+  end
+
+  @event <<0, 0, 0, 0, 3, 10, 73, 152, 0, 2, 147, 143, 249, 254, 114, 142, 0, 0, 14, 59>>
+  test "decode large transaction begin event" do
+    assert match?(
+             %Begin{
+               transaction_id: 3_643,
+               commit_timestamp: ~U[2022-12-24 11:28:57.229966Z],
+               final_lsn: {0, 51_005_848}
              },
              Begin.decode(@event)
            )
--- test/postgrex_wal/messages/update_test.exs
+++ test/postgrex_wal/messages/update_test.exs
@@ -21,4 +21,51 @@ defmodule PostgrexWal.Messages.UpdateTest do
              Update.decode(@event)
            )
   end
+
+  @doc """
+  UPDATE "users" SET "salary" = $1, "sex" = $2, "emo" = $3 WHERE "users"."id" = $4
+  [["salary", "12.34"], ["sex", true], ["emo", "add emo text"], ["id", 1]]
+  """
+  @event <<0, 0, 122, 7, 79, 0, 8, 116, 0, 0, 0, 1, 49, 116, 0, 0, 0, 3, 97, 98, 99, 116, 0, 0, 0,
+           2, 50, 50, 110, 116, 0, 0, 0, 60, 36, 50, 97, 36, 49, 50, 36, 115, 107, 53, 77, 118,
+           105, 116, 121, 51, 102, 67, 70, 56, 78, 47, 77, 67, 98, 67, 86, 103, 46, 115, 117, 70,
+           51, 78, 114, 57, 103, 82, 52, 46, 57, 99, 102, 74, 73, 105, 120, 53, 71, 104, 53, 83,
+           86, 104, 117, 46, 115, 122, 110, 83, 110, 110, 110, 78, 0, 8, 116, 0, 0, 0, 1, 49, 116,
+           0, 0, 0, 3, 97, 98, 99, 116, 0, 0, 0, 2, 50, 50, 110, 116, 0, 0, 0, 60, 36, 50, 97, 36,
+           49, 50, 36, 115, 107, 53, 77, 118, 105, 116, 121, 51, 102, 67, 70, 56, 78, 47, 77, 67,
+           98, 67, 86, 103, 46, 115, 117, 70, 51, 78, 114, 57, 103, 82, 52, 46, 57, 99, 102, 74,
+           73, 105, 120, 53, 71, 104, 53, 83, 86, 104, 117, 46, 115, 122, 110, 83, 116, 0, 0, 0,
+           5, 49, 50, 46, 51, 52, 116, 0, 0, 0, 1, 116, 116, 0, 0, 0, 12, 97, 100, 100, 32, 101,
+           109, 111, 32, 116, 101, 120, 116>>
+
+  test "decode users_one_row_update event" do
+    assert match?(
+             %Update{
+               changed_key_tuple_data: nil,
+               old_tuple_data: {
+                 {:text, "1"},
+                 {:text, "abc"},
+                 {:text, "22"},
+                 nil,
+                 {:text, "$2a$12$sk5Mvity3fCF8N/MCbCVg.suF3Nr9gR4.9cfJIix5Gh5SVhu.sznS"},
+                 nil,
+                 nil,
+                 nil
+               },
+               relation_oid: 31_239,
+               transaction_id: nil,
+               tuple_data: {
+                 {:text, "1"},
+                 {:text, "abc"},
+                 {:text, "22"},
+                 nil,
+                 {:text, "$2a$12$sk5Mvity3fCF8N/MCbCVg.suF3Nr9gR4.9cfJIix5Gh5SVhu.sznS"},
+                 {:text, "12.34"},
+                 {:text, "t"},
+                 {:text, "add emo text"}
+               }
+             },
+             Update.decode(@event)
+           )
+  end
 end

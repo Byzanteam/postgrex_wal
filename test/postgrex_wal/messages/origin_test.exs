0addac46c626402116032fe1fbe1b4b416b7c5ba
Jason <jminfo@126.com>
2022-12-24 17:42:53 +0800

    test: add 2 more test-cases. (origin, type)

---
+++ test/postgrex_wal/messages/origin_test.exs
@@ -0,0 +1,15 @@
+defmodule PostgrexWal.Messages.OriginTest do
+  use ExUnit.Case, async: true
+  alias PostgrexWal.Messages.Origin
+  @event <<0, 0, 0, 0, 2, 227, 251, 232, 109, 121, 95, 111, 114, 105, 103, 105, 110, 0>>
+
+  test "decode begin event" do
+    assert match?(
+             %Origin{
+               name: "my_origin",
+               commit_lsn: {0, 48_495_592}
+             },
+             Origin.decode(@event)
+           )
+  end
+end

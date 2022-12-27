8ecd071a33ff314349673c3fe48baf8e424562a2
Jason <jminfo@126.com>
2022-12-27 21:29:54 +0800

    feat: add `{:broadway, "~> 1.0"}` in deps.

--- mix.exs
+++ mix.exs
@@ -7,9 +7,16 @@ defmodule PostgrexWal.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps(),
+      aliases: aliases(),
+      preferred_cli_env: [cc: :test]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   def application do
     [extra_applications: [:logger]]
@@ -18,9 +25,22 @@ defmodule PostgrexWal.MixProject do
   defp deps do
     [
       {:postgrex, "~> 0.16"},
+      {:broadway, "~> 1.0"},
       {:typed_struct, "~> 0.3.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      cc: [
+        "format",
+        "format --check-formatted",
+        "credo --strict",
+        "dialyzer --format github",
+        "test"
+      ]
     ]
   end
 end

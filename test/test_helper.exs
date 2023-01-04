8dbe19047049c1b7376d0cbc2d49fd60745b921f
Jason <jminfo@126.com>
2023-01-04 13:14:19 +0800

    test: setup replication test env.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,10 @@
-ExUnit.start()
+ExUnit.start(assert_receive_timeout: 2_000, seed: 0)
+
+sql_test = """
+CREATE TABLE IF NOT EXISTS users (a int, b text);
+SELECT pg_create_logical_replication_slot('myslot5', 'pgoutput');
+CREATE PUBLICATION mypub5 FOR all tables;
+ALTER TABLE users REPLICA IDENTITY FULL;
+"""
+
+PostgrexWal.PSQL.cmd(sql_test)

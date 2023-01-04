00fb29c01b5e5ad871ae2def30c802cad27376e1
Jason <jminfo@126.com>
2023-01-04 13:36:38 +0800

    fix: separate sql to different transactions.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,9 +1,10 @@
 ExUnit.start(assert_receive_timeout: 2_000, seed: 0)
 
+PostgrexWal.PSQL.cmd("SELECT pg_create_logical_replication_slot('myslot5', 'pgoutput');")
+PostgrexWal.PSQL.cmd("CREATE PUBLICATION mypub5 FOR all tables;")
+
 sql_test = """
 CREATE TABLE IF NOT EXISTS users (a int, b text);
-SELECT pg_create_logical_replication_slot('myslot5', 'pgoutput');
-CREATE PUBLICATION mypub5 FOR all tables;
 ALTER TABLE users REPLICA IDENTITY FULL;
 """
 

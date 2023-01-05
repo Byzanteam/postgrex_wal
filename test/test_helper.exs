da02bb35e1f931cb84d2cab02eddaa4fdbdd5390
Jason <jminfo@126.com>
2023-01-05 17:28:43 +0800

    refactor: move some statements to `setup_all` callbacks.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,4 @@
-ExUnit.start(assert_receive_timeout: 2_000, seed: 0)
-
-PostgrexWal.PSQL.cmd("SELECT pg_create_logical_replication_slot('myslot5', 'pgoutput');")
-PostgrexWal.PSQL.cmd("CREATE PUBLICATION mypub5 FOR all tables;")
-
-sql_test = """
-CREATE TABLE IF NOT EXISTS users (a int, b text);
-ALTER TABLE users REPLICA IDENTITY FULL;
-"""
-
-PostgrexWal.PSQL.cmd(sql_test)
+ExUnit.start(
+  assert_receive_timeout: 3_000,
+  refute_receive_timeout: 1_000
+)

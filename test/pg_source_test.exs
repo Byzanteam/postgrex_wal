531dd47e0553a32d2651d4fa15123af9d67b7f3f
Jason <jminfo@126.com>
2023-01-05 17:29:53 +0800

    refactor: according to the review comments

--- test/pg_source_test.exs
+++ test/pg_source_test.exs
@@ -3,23 +3,59 @@ defmodule PgSourceTest do
   alias PostgrexWal.{PgSource, PgSourceRelayer, PSQL}
   alias PostgrexWal.Messages.{Begin, Commit, Insert}
 
-  @opts [
-    name: :my_pg_source,
-    publication_name: "mypub5",
-    slot_name: "myslot5",
-    database: "postgres",
-    username: "postgres"
-  ]
+  setup_all do
+    n = :rand.uniform(100_000)
+    slot_name = "slot_#{n}"
+    publication_name = "publication_#{n}"
+    table_name = "table_#{n}"
 
-  setup do
+    PSQL.cmd("SELECT pg_create_logical_replication_slot('#{slot_name}', 'pgoutput');")
+    PSQL.cmd("CREATE PUBLICATION #{publication_name} FOR all tables;")
+
+    sql_test = """
+    CREATE TABLE IF NOT EXISTS #{table_name} (a int, b text);
+    ALTER TABLE #{table_name} REPLICA IDENTITY FULL;
+    """
+
+    PSQL.cmd(sql_test)
+
+    on_exit(fn ->
+      PSQL.cmd("SELECT pg_drop_replication_slot('#{slot_name}');")
+      PSQL.cmd("DROP PUBLICATION #{publication_name};")
+      PSQL.cmd("DROP TABLE #{table_name};")
+    end)
+
     [
-      source: start_supervised!({PgSource, @opts}),
-      relayer: start_supervised!({PgSourceRelayer, {@opts[:name], self()}})
+      table_name: table_name,
+      opts: [
+        name: :my_pg_source,
+        publication_name: publication_name,
+        slot_name: slot_name,
+        database: "postgres",
+        username: "postgres"
+      ]
     ]
   end
 
-  test "shold receive replication events with ack", context do
-    PSQL.cmd("INSERT INTO users (a, b) VALUES (1, 'one');")
+  defp start_repl(context) do
+    start_supervised!({PgSource, context.opts})
+    start_supervised!({PgSourceRelayer, {context.opts[:name], self()}})
+  end
+
+  defp stop_repl do
+    stop_supervised!(PgSource)
+    stop_supervised!(PgSourceRelayer)
+  end
+
+  defp restart_repl(context) do
+    stop_repl()
+    start_repl(context)
+  end
+
+  test "pg logical replication ack test", context do
+    # shold receive replication events (with consume ack)
+    start_repl(context)
+    PSQL.cmd("INSERT INTO #{context.table_name} (a, b) VALUES (1, 'one');")
 
     assert_receive [
       %Begin{},
@@ -27,9 +63,9 @@ defmodule PgSourceTest do
       %Commit{end_lsn: lsn}
     ]
 
-    PgSource.ack(context[:source], lsn)
+    PgSource.ack(context.opts[:name], lsn)
 
-    PSQL.cmd("INSERT INTO users (a, b) VALUES (2, 'two');")
+    PSQL.cmd("INSERT INTO #{context.table_name} (a, b) VALUES (2, 'two');")
 
     assert_receive [
       %Begin{},
@@ -37,11 +73,20 @@ defmodule PgSourceTest do
       %Commit{end_lsn: lsn}
     ]
 
-    PgSource.ack(context[:source], lsn)
-  end
+    PgSource.ack(context.opts[:name], lsn)
 
-  test "without ack" do
-    PSQL.cmd("INSERT INTO users (a, b) VALUES (3, 'three');")
+    # "should not receive any already acked messages"
+    restart_repl(context)
+
+    refute_receive [
+      %Begin{},
+      %Insert{},
+      %Commit{}
+    ]
+
+    # "without ack"
+    restart_repl(context)
+    PSQL.cmd("INSERT INTO #{context.table_name} (a, b) VALUES (3, 'three');")
 
     assert_receive [
       %Begin{},
@@ -49,33 +94,49 @@ defmodule PgSourceTest do
       %Commit{}
     ]
 
-    PSQL.cmd("INSERT INTO users (a, b) VALUES (4, 'four');")
+    PSQL.cmd("INSERT INTO #{context.table_name} (a, b) VALUES (4, 'four');")
 
     assert_receive [
       %Begin{},
       %Insert{tuple_data: [text: "4", text: "four"]},
       %Commit{}
     ]
-  end
 
-  test "still receive un-acked message", context do
-    PSQL.cmd("INSERT INTO users (a, b) VALUES (5, 'five');")
+    # "still can receive un-acked message"
+    restart_repl(context)
+    PSQL.cmd("INSERT INTO #{context.table_name} (a, b) VALUES (5, 'five');")
 
+    # acked message should not received again
+    refute_receive [
+      %Begin{},
+      %Insert{tuple_data: [text: "1", text: "one"]},
+      %Commit{}
+    ]
+
+    # acked message should not received again
+    refute_receive [
+      %Begin{},
+      %Insert{tuple_data: [text: "2", text: "two"]},
+      %Commit{}
+    ]
+
+    # un-acked message should received again
     assert_receive [
       %Begin{},
       %Insert{tuple_data: [text: "3", text: "three"]},
       %Commit{end_lsn: lsn}
     ]
 
-    PgSource.ack(context[:source], lsn)
+    PgSource.ack(context.opts[:name], lsn)
 
+    # un-acked message should received again
     assert_receive [
       %Begin{},
       %Insert{tuple_data: [text: "4", text: "four"]},
       %Commit{end_lsn: lsn}
     ]
 
-    PgSource.ack(context[:source], lsn)
+    PgSource.ack(context.opts[:name], lsn)
 
     assert_receive [
       %Begin{},
@@ -83,6 +144,16 @@ defmodule PgSourceTest do
       %Commit{end_lsn: lsn}
     ]
 
-    PgSource.ack(context[:source], lsn)
+    PgSource.ack(context.opts[:name], lsn)
+
+    restart_repl(context)
+    # "due to all acked, should not receive anymore" d
+    refute_receive [
+      %Begin{},
+      %Insert{},
+      %Commit{}
+    ]
+
+    stop_repl()
   end
 end

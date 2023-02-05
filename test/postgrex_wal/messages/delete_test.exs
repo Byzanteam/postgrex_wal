382f91dd24c06bf569bab85637036ada4807a9db
Jason <jminfo@126.com>
2023-02-05 08:33:17 +0800

    refactor: move stream related test to `pg_source_util_test.exs`

--- test/postgrex_wal/messages/delete_test.exs
+++ test/postgrex_wal/messages/delete_test.exs
@@ -1,7 +1,6 @@
 defmodule PostgrexWal.Messages.DeleteTest do
   use ExUnit.Case, async: true
   alias PostgrexWal.Messages.Delete
-  alias PostgrexWal.PgSourceUtil
 
   @event <<0, 0, 89, 103, 79, 0, 7, 116, 0, 0, 0, 9, 57, 56, 48, 49, 57, 49, 48, 50, 57, 116, 0,
            0, 0, 8, 116, 105, 116, 108, 101, 50, 50, 50, 116, 0, 0, 0, 1, 50, 110, 110, 110, 110>>
@@ -46,26 +45,6 @@ defmodule PostgrexWal.Messages.DeleteTest do
                transaction_id: nil
              },
              Delete.decode(@event)
-           )
-  end
-
-  test "decode steamed delete event" do
-    assert match?(
-             %Delete{
-               changed_key_tuple_data: [
-                 {:text, "980191029"},
-                 {:text, "title222"},
-                 {:text, "2"},
-                 nil,
-                 nil,
-                 nil,
-                 nil
-               ],
-               old_tuple_data: nil,
-               relation_oid: 22_887,
-               transaction_id: 123
-             },
-             PgSourceUtil.decode({:in_stream, <<?D, 123::32, @event>>})
            )
   end
 end
--- test/postgrex_wal/messages/type_test.exs
+++ test/postgrex_wal/messages/type_test.exs
@@ -1,7 +1,6 @@
 defmodule PostgrexWal.Messages.TypeTest do
   use ExUnit.Case, async: true
   alias PostgrexWal.Messages.Type
-  alias PostgrexWal.PgSourceUtil
   @event <<0, 0, 121, 228, 112, 117, 98, 108, 105, 99, 0, 109, 121, 95, 116, 121, 112, 101, 0>>
 
   test "decode type event" do
@@ -13,18 +12,6 @@ defmodule PostgrexWal.Messages.TypeTest do
                type_oid: 31_204
              },
              Type.decode(@event)
-           )
-  end
-
-  test "decode steamed type event" do
-    assert match?(
-             %Type{
-               transaction_id: 123,
-               namespace: "public",
-               type_name: "my_type",
-               type_oid: 31_204
-             },
-             PgSourceUtil.decode({:in_stream, <<?Y, 123::32, @event>>})
            )
   end
 end

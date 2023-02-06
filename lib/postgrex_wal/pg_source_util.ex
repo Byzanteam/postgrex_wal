de35b810e54f9d7cf0849ccf2eed2ead07dc0f2b
Jason <jminfo@126.com>
2023-02-06 09:48:10 +0800

    refactor: `PgSourceUtil/decode_wal/2`

--- lib/postgrex_wal/pg_source_util.ex
+++ lib/postgrex_wal/pg_source_util.ex
@@ -4,6 +4,14 @@ defmodule PostgrexWal.PgSourceUtil do
   """
 
   require Logger
+  alias PostgrexWal.PgSource
+
+  @doc """
+  The logical replication protocol sends individual transactions one by one.
+  This means that all messages between a pair of Begin and Commit messages belong to the same transaction.
+  It also sends changes of large in-progress transactions between a pair of Stream Start and Stream Stop messages.
+  The last stream of such a transaction contains Stream Commit or Stream Abort message.
+  """
 
   @modules %{
     Begin => ?B,
@@ -21,52 +29,34 @@ defmodule PostgrexWal.PgSourceUtil do
     Type => ?Y,
     Update => ?U
   }
-
-  @doc """
-  The logical replication protocol sends individual transactions one by one.
-  This means that all messages between a pair of Begin and Commit messages belong to the same transaction.
-  It also sends changes of large in-progress transactions between a pair of Stream Start and Stream Stop messages.
-  The last stream of such a transaction contains Stream Commit or Stream Abort message.
-  """
-
   @stream_start @modules[StreamStart]
   @stream_stop @modules[StreamStop]
-  @spec decode_wal(binary(), PostgrexWal.PgSource.t()) :: {struct(), PostgrexWal.PgSource.t()}
-  def decode_wal(<<key::8, rest::binary>> = event, state) do
-    in_stream? =
-      case key do
-        @stream_start ->
-          state.in_stream? && Logger.error("stream flag consecutively true")
-          true
 
-        @stream_stop ->
-          state.in_stream? || Logger.error("stream flag consecutively false")
-          false
+  @spec decode_wal(binary(), PgSource.t()) :: {struct(), PgSource.t()}
+  def decode_wal(<<@stream_start::8, _rest::binary>> = event, state) do
+    if state.in_stream?, do: Logger.error("stream flag consecutively true")
+    {decode(event), %{state | in_stream?: true}}
+  end
 
-        _ ->
-          state.in_stream?
-      end
+  def decode_wal(<<@stream_stop::8, _rest::binary>> = event, state) do
+    unless state.in_stream?, do: Logger.error("stream flag consecutively false")
+    {decode(event), %{state | in_stream?: false}}
+  end
 
-    message =
-      if in_stream? and streamable?(key) do
-        <<transaction_id::32, rest::binary>> = rest
-        decode(<<key::8>> <> rest) |> struct(transaction_id: transaction_id)
-      else
-        decode(event)
-      end
+  @streamable_modules [Delete, Insert, Message, Relation, Truncate, Type, Update]
+  @streamable_keys @modules |> Map.take(@streamable_modules) |> Map.values()
+  def decode_wal(<<key::8, transaction_id::32, rest::binary>>, %{in_stream?: true} = state)
+      when key in @streamable_keys do
+    message = decode(<<key::8>> <> rest) |> struct!(transaction_id: transaction_id)
+    {message, state}
+  end
 
-    {message, %{state | in_stream?: in_stream?}}
+  def decode_wal(event, state) do
+    {decode(event), state}
   end
 
   for {module, key} <- @modules do
     m = Module.concat(PostgrexWal.Messages, module)
     defp decode(<<unquote(key)::8, payload::binary>>), do: unquote(m).decode(payload)
   end
-
-  @streamable_modules [Delete, Insert, Message, Relation, Truncate, Type, Update]
-  for {_module, key} <- Map.take(@modules, @streamable_modules) do
-    defp streamable?(unquote(key)), do: true
-  end
-
-  defp streamable?(_key), do: false
 end
